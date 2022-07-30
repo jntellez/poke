bd079d7ea36feac33db6a04b0d24ff59a09833e5
Juan Tellez <juantellez916@gmail.com>
2022-07-29 22:06:09 -0500

    modificando titulo

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Pokemon = ({ pokemon }) => {
 export default function Pokemones({ pokemones }) {
   return (
     <div>
-      <p>Pokemones</p>
+      <p>Mi app de pokemones</p>
       <ul>
         {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
       </ul>
